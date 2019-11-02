6e984d204c0c042d77eac50388efcd4a7318dfd1
AnaSenica <ana.senica1999@gmail.com>
2019-11-02 02:24:27 +0100

    Končala z regularnimi izrazi, podatke dala v seznam

--- knjige.py
+++ knjige.py
@@ -10,7 +10,7 @@ mapa = 'knjige'
 # ime datoteke, v katero bom shranila glavno stran
 glavna_stran = 'knjge_stran.html'
 # ime CSV datoteke, v katero bom shranila podatke
-csv_datoteka = 'knjige.csv'
+csv_datoteka = 'filmi_po_knjigah.csv'
 
 def download_url_to_string(url):
     """Funkcija kot argument sprejme niz in puskuša vrniti vsebino te spletne
@@ -42,29 +42,8 @@ def save_frontpage(page, directory, filename):
     return
 
 #save_frontpage(knjige_url, mapa, glavna_stran)
-#####################################################################################
-# od tu naprej moram popraviti
-#####################################################################################
-# page_to_ads = page_to_books
-# get_dict_from_ad_block = get_dict_from_book_block
-# ads_from_file = books_from_file
-# ads_frontpage = books_frontpage
 
 
-def read_file_to_string(directory, filename):
-    """Funkcija vrne celotno vsebino datoteke "directory"/"filename" kot niz"""
-    path = os.path.join(directory, filename)
-    with open(path, 'r', encoding='utf8') as datoteka1:
-        return datoteka1.read()
-
-
-def page_to_books(page_content):
-    """Funkcija poišče posamezne knjige, ki se nahajajo v spletni strani in
-    vrne njih seznam"""
-    izraz = re.compile(r'<tr>.*?<td><i><a (.*?)</td></tr>', re.DOTALL)
-    return [m.group(0) for m in re.finditer(izraz, page_content)]
-
-# Da preverim delovanje te funkcije, vstavim npr. tole: niz = ' <tr><td><i><a href="/wiki/The_Red_Danube" title="The Red Danube">The Red Danube</a></i></td><td>1949</td><td><a href="/wiki/George_Sidney" title="George Sidney">George Sidney</a></td><td>US</td><td><i><a href="/wiki/Vespers_in_Vienna" title="Vespers in Vienna">Vespers in Vienna</a></i></td><td><a href="/wiki/Bruce_Marshall" title="Bruce Marshall">Bruce Marshall</a></td><td>1947</td><td>Novel</td></tr><tr>'
 
 vzorec_filma = re.compile(
     r'<td><i><a.*?title=".*?">(?P<film>.*?)</a></i>.*?</td>.*?'
@@ -79,43 +58,8 @@ vzorec_filma = re.compile(
     re.DOTALL
 )
 
-
-
-#def get_dict_from_book_block(blok):
-#    """Funkcija iz niza za posamezno knjigo izlušči podatke o ........ imenu, ceni
-#    in opisu ter vrne slovar, ki vsebuje ustrezne podatke
-#    """
-##    izraz = vzorec_filma
- #   podatki = re.search(izraz, blok)
- #   slovar = podatki.groupdict()
-  #  return slovar
-
-
 #[get_dict_from_book_block(knjiga) for knjiga in page_to_books(read_file_to_string(mapa, 'filmi3.html'))]
 
-# Definirajte funkcijo, ki sprejme ime in lokacijo datoteke, ki vsebuje
-# besedilo spletne strani, in vrne seznam slovarjev, ki vsebujejo podatke o
-# vseh oglasih strani.
-
-def books_frontpage():
-    return books_from_file(glavna_stran, mapa)
-
-
-
-
-def books_from_file(ime_datoteke, lokacija_datoteke):
-    """Funkcija prebere podatke v datoteki "directory"/"filename" in jih
-    pretvori (razčleni) v pripadajoč seznam slovarjev za vsako knjigo posebej."""
-    stran = read_file_to_string(lokacija_datoteke, ime_datoteke)
-    knjige = page_to_books(stran)
-    seznam = [popravi_podatke(knjiga) for knjiga in knjige]
-    return seznam
-
-def mala_books_frontpage():
-    return books_from_file('filmi3.html', mapa)
-
-#mala_books_frontpage()
-#books_from_file('filmi3.html', mapa)
 
 
 vzorec_z_a = re.compile(
@@ -158,7 +102,10 @@ vzorec_brez_sup = re.compile(
     r'(.+?)<sup.*?>.*?</sup>',
     flags=re.DOTALL
 )
-
+vzorec_brez_krnekej = re.compile(
+    r'(.+?)&#160;: roman.*?',
+    flags=re.DOTALL
+)
 def pomozna_funkcija1(film, kategorija):
     film = film
     if ' &amp; ' not in film[kategorija]:
@@ -219,11 +166,37 @@ def pomozna_funkcija_leto_knjige(film, kategorija):
         popravljene_letnice = film[kategorija].replace('?', '0')
         film[kategorija] = popravljene_letnice[:4]
     return film
-
-
     
+def pomozna_funkcija_knjiga(film, kategorija):
+    film = film
+    if film[kategorija] == '?':
+        film[kategorija] = None
+    else:
+        if '<sup' in film[kategorija]:
+            film[kategorija] = film[kategorija][:5] + film[kategorija][-8:]
+        elif '&#160;: roman' in film[kategorija]:
+            film[kategorija] = vzorec_brez_krnekej.sub(r'\1', film[kategorija])
+    return film
+
+
+
 
 ########################################################################
+
+def read_file_to_string(directory, filename):
+    """Funkcija vrne celotno vsebino datoteke "directory"/"filename" kot niz"""
+    path = os.path.join(directory, filename)
+    with open(path, 'r', encoding='utf8') as datoteka1:
+        return datoteka1.read()
+
+
+def page_to_books(page_content):
+    """Funkcija poišče posamezne knjige, ki se nahajajo v spletni strani in
+    vrne njih seznam"""
+    izraz = re.compile(r'<tr>.*?<td><i><a (.*?)</td></tr>', re.DOTALL)
+    return [m.group(0) for m in re.finditer(izraz, page_content)]
+
+
 def popravi_podatke(blok):
     film = vzorec_filma.search(blok).groupdict()
     pomozna_funkcija1(film, 'drzave')
@@ -231,6 +204,62 @@ def popravi_podatke(blok):
     pomozna_funkcija1(film, 'reziser')
     pomozna_funkcija_zvrst(film, 'zvrst')
     pomozna_funkcija_leto_knjige(film, 'leto_izida_knjige')
-    print(film['leto_izida_knjige'])
-    #return film
+    pomozna_funkcija_knjiga(film, 'knjiga')
+    return film
 
+
+def books_from_file(ime_datoteke, lokacija_datoteke):
+    """Funkcija prebere podatke v datoteki "directory"/"filename" in jih
+    pretvori (razčleni) v pripadajoč seznam slovarjev za vsako knjigo posebej."""
+    stran = read_file_to_string(lokacija_datoteke, ime_datoteke)
+    knjige = page_to_books(stran)
+    seznam = [popravi_podatke(knjiga) for knjiga in knjige]
+    return seznam
+
+
+def zapisi_seznam_filmov():
+    seznam = books_from_file(glavna_stran, mapa)
+    nov_seznam = []
+    for slovar in seznam:
+        if slovar not in nov_seznam:
+            nov_seznam.append(slovar)
+        else:
+            pass
+    stevilo = 1
+    for slovar in nov_seznam:
+        slovar['id'] = stevilo
+        stevilo += 1
+    return nov_seznam
+
+
+
+def izloci_gnezdene_podatke(filmi):
+    reziser, avtor, zvrst, drzave = [], [], [], []
+    for film in filmi:
+        for z in film.pop('zvrst'):
+            zvrst.append({'film': film['id'], 'zvrst': z})
+        for a in film.pop('avtor'):
+            avtor.append({'film': film['id'], 'avtor': a})
+        for r in film.pop('reziser'):
+            reziser.append({'film': film['id'], 'reziser': r})
+        for d in film.pop('drzave'):
+            drzave.append({'film': film['id'], 'drzave': d})
+    return reziser, avtor, zvrst, drzave
+
+def pripravi_imenik(ime_datoteke):
+    '''Če še ne obstaja, pripravi prazen imenik za dano datoteko.'''
+    imenik = os.path.dirname(ime_datoteke)
+    if imenik:
+        os.makedirs(imenik, exist_ok=True)
+
+def zapisi_csv(slovarji, imena_polj, ime_datoteke):
+    '''Iz seznama slovarjev ustvari CSV datoteko z glavo.'''
+    pripravi_imenik(ime_datoteke)
+    with open(ime_datoteke, 'w', encoding='utf-8') as csv_datoteka:
+        writer = csv.DictWriter(csv_datoteka, fieldnames=imena_polj)
+        writer.writeheader()
+        for slovar in slovarji:
+            writer.writerow(slovar)
+
+
+#zapisi_csv(zapisi_seznam_filmov(), ['id', 'film', 'leto_filma', 'knjiga', 'leto_izida_knjige'], csv_datoteka)
